d6f5ababf36ea90767b92237ee409d5df4c048e5
Alona Dmytrenko <alenkadmitrenko@yahoo.com.ua>
2018-04-25 15:45:10 +0300

    Update script.js

--- Haha/Task4/script.js
+++ Haha/Task4/script.js
@@ -1,7 +1,7 @@
 var date = prompt('Enter the date','17:00');
-if (date == string) {
-  prompt('Enter the date again','17:00'); 
-} 
+do {
+   data = prompt('Enter the date','17:00');
+} while (isNaN(data));
 var name = prompt('Enter the name','Petro');
 
 var city = prompt('Enter the city','Milan');
