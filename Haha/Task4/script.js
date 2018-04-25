7730d054d5112865058dab03cac00e046e1cc24a
Alona Dmytrenko <alenkadmitrenko@yahoo.com.ua>
2018-04-25 16:06:32 +0300

    Update script.js

--- Haha/Task4/script.js
+++ Haha/Task4/script.js
@@ -2,7 +2,7 @@ while(true) {
 var date = prompt('Enter the date','today 17:00');
 var name = prompt ('Enter the name','Petro');
 var city = prompt('Enter the city','Milan');
-var regExp_Name_City = /\D/;
+var regExp_Name_City = /\d/;
 	if (!regExp_Name_City.test(name) || !regExp_Name_City.test(city)) {
 		alert("Try again");
 	} else {
