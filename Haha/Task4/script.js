ea1a8a63dbd97f11df3e6b355ea58fc00ba89ae3
Alona Dmytrenko <alenkadmitrenko@yahoo.com.ua>
2018-04-25 15:41:05 +0300

    Update script.js

--- Haha/Task4/script.js
+++ Haha/Task4/script.js
@@ -1,30 +1,10 @@
-var date;
-if (typeof date !== 'number') {
-  return prompt('Enter the date again','17:00'); 
-} else {
-  prompt('Enter the date','today at 17:00');
-}
+var date = prompt('Enter the date','17:00');
+if (date !== 'number') {
+  prompt('Enter the date again','17:00'); 
+} 
 var name = prompt('Enter the name','Petro');
 
 var city = prompt('Enter the city','Milan');
 
 alert(name + ' has a meeting ' + date +  ' somewhere in ' + city + '.');
 
-
-// var date = prompt('Enter the date','today at 17:00');
-// var name = prompt('Enter the name','Petro');
-// var city = prompt('Enter the city','Milan');
-
-// if (typeof date !== 'number') {
-//   return false;
-// } prompt('Enter the date again','17:00');
-
-// if (typeof name !== 'string') {
-//   return false;
-// } prompt('Enter the name again','Petro');
-
-// if (typeof city !== 'string') {
-//   return false;
-// } prompt('Enter the city again','Milan');
-
-// alert(name + ' has a meeting ' + date +  ' somewhere in ' + city + '.');
