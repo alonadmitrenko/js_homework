562482be5567525908703ce3accac7dee66290ec
Alona Dmytrenko <alenkadmitrenko@yahoo.com.ua>
2018-05-06 18:17:25 +0300

    Update script.js

--- Hard/Task2/script.js
+++ Hard/Task2/script.js
@@ -1,17 +1,15 @@
 function validate(properties, values) {
     for (i = 0; i < properties.length; i++) {
         if (values[properties[i]] == undefined) {
-            throw console.error('Propetry ' + '"' + properties[i] + '"' + ' is not enter!');
+            throw new Error("Propetry " + properties[i] + " is undefined");
         }
     }
     return true;
+
 }
 try {
-    console.log(validate(['name', 'surname'], { name: 'ALona', surname: 'DMytrenko' }));
-    console.log(validate(['name', 'surname', 'age'], { name: 'ALona', surname: 'DMytrenko' }));
-} catch (e) {
-
-} {
-    console.log();
+    console.log(validate(["name", "surname"], { name: "Roman", surname: "Rodomansky" }));
+    console.log(validate(["name", "surname", "age"], { name: "Roman", surname: "Rodomansky" }));
+} catch (error) {
+    console.log(error.message);
 }
-
