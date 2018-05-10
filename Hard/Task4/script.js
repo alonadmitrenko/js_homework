80c79be80642c9dd24f3ffd03075db4ddb3bc684
Alona Dmytrenko <alenkadmitrenko@yahoo.com.ua>
2018-05-10 20:59:39 +0300

    Update script.js
    fixed errors

--- Hard/Task4/script.js
+++ Hard/Task4/script.js
@@ -2,8 +2,9 @@ var arr = [2,2,4,5,6,7,7];
 var sorted_arr = arr.slice().sort();
 var results = [];
 for (var i = 0; i < sorted_arr.length - 1; i++) {
-    if (sorted_arr[i + 1] == sorted_arr[i]) {
+    if (!(results.includes(sorted_arr[i]))) {
         results.push(sorted_arr[i]);
     }
 }
 console.log(results);
+
