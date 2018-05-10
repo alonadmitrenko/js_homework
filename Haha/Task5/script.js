23efe59a3b6cffd04081eccdc6521439a18bf2b5
Alona Dmytrenko <alenkadmitrenko@yahoo.com.ua>
2018-05-10 20:47:51 +0300

    Update script.js
    fixed errors

--- Haha/Task5/script.js
+++ Haha/Task5/script.js
@@ -1,13 +1,17 @@
-function currencyConverter(quantity, currency) {
-    if (currency == 'UAN') {
-        return quantity * 25.95;
+const VAL = 26;
+function currencyConverter(number, currency) {
+    if (currency == 'UAH') {
+        return number * VAL;
     } else if (currency == 'USD') {
-        return quantity / 26.2;
+        return number / VAL;
     } else {
-        return 'We have a currency converter ONLY from USD to UAH and reverse.';
+         return 'We have a currency converter ONLY from USD to UAH and reverse.';
     }
 }
-console.log(currencyConverter(100, 'UAN'));
-console.log(currencyConverter(5000, 'USD'));
-console.log(currencyConverter(500, 'EUR'));
-console.log(currencyConverter(500));
+
+console.log(currencyConverter(100, 'UAH'));
+console.log(currencyConverter(2600, 'USD'));
+console.log(currencyConverter(100));
+
+
+
